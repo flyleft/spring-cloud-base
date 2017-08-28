e985faf7ba4de79c8b62145e9aaa1c9a098eb1a4
左志鹏 <jcalaz@163.com>
2017-08-28 15:59:20 +0800

    [ADD] 初始化项目

---
+++ register/src/main/java/me/jcala/register/Application.java
@@ -0,0 +1,12 @@
+package me.jcala.register;
+
+/**
+ * Created by zhipeng.zuo on 2017/8/28.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
+public class Application {
+}
