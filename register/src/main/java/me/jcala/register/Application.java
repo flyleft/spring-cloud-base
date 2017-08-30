555bc7126b6adc974dce200b6ce5229770a7f068
左志鹏 <jcalaz@163.com>
2017-08-30 09:16:35 +0800

    [ADD] 添加了可运行的client-a,register,consumer-by-ribbon,consumer-by-ribbon-and-hytrix模块

--- register/src/main/java/me/jcala/register/Application.java
+++ register/src/main/java/me/jcala/register/Application.java
@@ -1,12 +1,16 @@
 package me.jcala.register;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Created by zhipeng.zuo on 2017/8/28.
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@EnableEurekaClient
-@RestController
+@SpringBootApplication
+@EnableEurekaServer
 public class Application {
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(Application.class).web(true).run(args);
+  }
 }
