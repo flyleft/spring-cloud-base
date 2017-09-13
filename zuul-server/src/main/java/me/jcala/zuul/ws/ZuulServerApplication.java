7018c5cc6eca46a82dfff9c1e99f7d69cb5a75d0
左志鹏 <zhipeng.zuo@hand-china.com>
2017-09-13 14:15:18 +0800

    [ADD] 添加了zuul-websocket的服务

---
+++ zuul-server/src/main/java/me/jcala/zuul/ws/ZuulServerApplication.java
@@ -0,0 +1,20 @@
+package me.jcala.zuul.ws;
+
+import me.jcala.zuul.ws.socket.EnableZuulWebsocket;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by zhipeng.zuo on 2017/8/28.
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableZuulWebsocket
+@SpringBootApplication
+public class ZuulServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ZuulServerApplication.class, args);
+  }
+}
