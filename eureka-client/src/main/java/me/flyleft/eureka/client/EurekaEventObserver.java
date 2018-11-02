53854fa8c40bf63f9f1d9e3512f8585e266ed86c
flyleft <jcalaz@163.com>
2018-11-02 16:16:25 +0800

    [FIX] 修复SynchronizedLinkedList未覆盖方法造成的异常

--- eureka-client/src/main/java/me/flyleft/eureka/client/EurekaEventObserver.java
+++ eureka-client/src/main/java/me/flyleft/eureka/client/EurekaEventObserver.java
@@ -7,17 +7,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomAbstractEurekaEventObserver extends AbstractEurekaEventObserver {
+public class EurekaEventObserver extends AbstractEurekaEventObserver {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomAbstractEurekaEventObserver.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EurekaEventObserver.class);
 
-    public CustomAbstractEurekaEventObserver() {
-        super(10, 5);
+    public EurekaEventObserver() {
+        super(5, 5, 2 << 8);
     }
 
     @Override
     public void receiveUpEvent(EurekaEventPayload payload) {
         LOGGER.info("======UP, id: {}", payload.getId());
+        if (payload.getAppName().equalsIgnoreCase("eureka-client")) {
+            throw new RuntimeException("测试异常");
+        }
     }
 
     @Override
